efda4a808a59f052c290a76affde49b525ef61a1
Jgayle <Jgayle99@gmail.com>
2020-01-25 09:45:02 -0500

    Implemented Spring XML configuration

--- src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -1,17 +1,15 @@
 package guru.springframework.joke.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 
 /**
  * Created by Joel Gayle on 25 Jan, 2020
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+   // @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return  new ChuckNorrisQuotes();
     }
